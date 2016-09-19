fcb49c3bff29807d07e3b1620e4aa631db0b9ed2
HawiCaesar <brian.odhiambo932@gmail.com>
2016-09-19 17:02:17 +0300

    primary 2014 data,added map4.html

---
+++ map4-data.php
@@ -0,0 +1,243 @@
+<?php
+
+$a= array(
+		array(
+			"code" =>  "Nairobi",	
+			"value" => 61126,
+			
+			),
+		array(
+			"code" =>  "Mombasa",	
+			"value" =>  17072,
+			
+			),
+		array(
+			"code" =>  "Kiambu",	
+			"value" =>  38333,
+			
+			),
+		array(
+			"code" => "Kisumu",
+			"value" =>  36080,
+			
+			),
+		array(
+			"code" => "Nakuru",	
+			"value" =>  56215,
+			
+			),
+		array(
+			"code" => "Lamu",	
+			"value" => 4206,
+			
+			),
+		array(
+			"code" => "Machakos",	
+			"value" => 36521,
+			
+			),
+		array(
+			"code" => "Kajiado",	
+			"value" => 21439,
+			
+			),
+		array(
+			"code" => "Nyeri",
+			"value" => 16377,
+			
+			),
+		array(
+			"code" => "Kirinyaga",	
+			"value" => 13505,
+			
+			),
+		array(
+			"code" => "Uasin Gishu",	
+			"value" => 24494,
+			
+			),
+		array(
+			"code" => "Isiolo",	
+			"value" => 5867,
+			
+			),
+		array(
+			"code" => "Vihiga",
+			"value" => 25037,
+			
+			),
+		array(
+			"code" => "Migori",
+			"value" => 42453,
+			
+			),
+		array(
+			"code" => "Murang'a",	
+			"value" => 25102,
+			
+			),
+		array(
+			"code" => "Embu",	
+			"value" => 14963,
+			
+			),
+		array(
+			"code" => "Nyandarua",	
+			"value" => 17660,
+			
+			),
+		array(
+			"code" => "Kericho",	
+			"value" => 31104,
+			
+			),
+		array(
+			"code" => "Tharaka Nithi",	
+			"value" => 13777,
+			
+			),
+		array(
+			"code" => "Siaya",	
+			"value" => 36081,
+			
+			),
+		array(
+			"code" => "Meru",	
+			"value" => 48964,
+			
+			),
+		array(
+			"code" => "Bungoma",	
+			"value" => 68314,
+			
+			),
+		array(
+			"code" => "Trans Nzoia",	
+			"value" => 32551,
+			
+			),
+		array(
+			"code" => "Homa Bay",	
+			"value" => 43796,
+			
+			),
+		array(
+			"code" => "Kisii",	
+			"value" => 43769,
+			
+			),
+		array(
+			"code" => "Kilifi",
+			"value" => 40969,
+			
+			),
+		array(
+			"code" => "Nandi",	
+			"value" => 32384,
+			
+			),
+		array(
+			"code" => "Taita Taveta",
+			"value" => 8685,
+			
+			),
+		array(
+			"code" => "Kakamega",	
+			"value" => 77579,
+			
+			),
+		array(
+			"code" => "Busia",
+			"value" => 37041,
+			
+			),
+		array(
+			"code" => "Nyamira",	
+			"value" => 20439,
+			
+			),
+		array(
+			"code" => "Laikipia",	
+			"value" => 12699,
+			
+			),
+		array(
+			"code" => "Elgeyo Marakwet",	
+			"value" => 15567,
+			
+			),
+		array(
+			"code" => "Narok",
+			"value" => 37870,
+			
+			),
+		array(
+			"code" => "Baringo",	
+			"value" => 19637,
+			
+			),
+		array(
+			"code" => "Bomet",	
+			"value" => 31793,
+			
+			),
+		array(
+			"code" => "Makueni",	
+			"value" => 36629,
+			
+			),
+		array(
+			"code" => "Kitui",	
+			"value" => 47523,
+			
+			),
+		array(
+			"code" => "Tana River",
+			"value" => 8271,
+			
+			),
+		array(
+			"code" => "Kwale",	
+			"value" => 27001,
+			
+			),
+		array(
+			"code" => "Garissa",	
+			"value" => 27525,
+			
+			),
+		array(
+			"code" => "Marsabit",	
+			"value" => 10024,
+			
+			),
+		array(
+			"code" => "West Pokot",
+			"value" => 28589,
+			
+			),
+		array(
+			"code" => "Samburu",	
+			"value" => 8395,
+			
+			),
+		array(
+			"code" => "Turkana",	
+			"value" => 48850,
+			
+			),
+		array(
+			"code" => "Mandera",	
+			"value" => 17207,
+			
+			),
+		array(
+			"code" => "Wajir",	
+			"value" => 13123,
+			
+			)
+);
+
+
+echo json_encode($a); 
+?>
